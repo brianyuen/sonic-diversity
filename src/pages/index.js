81e03feb619255d69e14151440ba2ebccb87ba0c
Brian Yuen <brianyuen@outlook.com>
2021-01-14 22:50:43 -0800

    remove unnecessary elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -7,7 +8,8 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Stand for Sonic Diversity" />
+    <Helmet bodyAttributes={{class: 'home'}} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
