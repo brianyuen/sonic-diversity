c00f9f3d8af9b868dc5ef986218854917b8ed71f
Brian Yuen <brianyuen@outlook.com>
2021-01-15 15:11:03 -0800

    configure gatsby-image for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,43 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import { Helmet } from "react-helmet"
+import Img from 'gatsby-image'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import home from '../scss/home.module.scss'
 
-const IndexPage = () => (
+export const data = graphql`
+  query {
+    imageOne: file(relativePath: { eq: "lgo-stand.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 960) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+const IndexPage = (props) => (
   <Layout>
     <SEO title="Stand for Sonic Diversity" />
     <Helmet bodyAttributes={{class: 'home'}} />
     <div className={home.wrapper}>
-      <section className={home.firstCol}>
-        <h1>LOGO</h1>
+      <section id="firstCol" className={home.firstCol}>
+        <Img fluid={props.data.imageOne.childImageSharp.fluid} className={home.logo} alt="Logo" />
       </section>
-      <section className={home.secondCol}>
-        intro
-        <Link to="/page-2/">Go to page 2</Link>
+      <section id="secondCol" className={home.secondCol}>
+        <h1>Lines have been drawn that perpetuate racism in every industry.</h1>
+        <p>Scelerisque ligula lorem convallis placerat pharetra fringilla curae vitae ullamcorper augue inceptos elit vehicula est, aenean aliquam sociis quisque tristique sed imperdiet sapien tempus suspendisse pretium erat. Id venenatis litora fermentum lorem diam dolor lacus quis</p>
+
+        <p>Nunc feugiat platea augue fames etiam id elit ornare, pellentesque aliquet sociis morbi sit in libero pulvinar, condimentum proin vivamus urna ad suspendisse at. Tempus blandit nam magnis vel taciti venenatis lobortis ante, ornare est mattis pellentesque sem dictum bibendum eu</p>
+
+        <ul className={home.introCta}>
+          <li><Link className="button primary" to="/page-2/">Start your stand</Link></li>
+          <li><Link className="button secondary" to="/page-2/">What is the sonic color line</Link></li>
+        </ul>
+
       </section>
     </div>
   </Layout>
