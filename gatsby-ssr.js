8f5720faed5bc04e5cde97b13e111fe82eeaf4b2
Brian Yuen <brianyuen@outlook.com>
2021-01-14 19:15:09 -0800

    setup sass and js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from 'react'
 
-// You can delete this file if you're not using it
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key="/js/site.js"
+      src="/js/site.js"
+    />
+  ])
+}
--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import header from '../scss/header.module.scss'
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -16,16 +18,8 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+      <h1 className={header.header}>
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
   </header>
