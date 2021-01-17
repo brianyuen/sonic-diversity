c834eab58b750e87bcbcb8164ba3c7101d77e5bb
Brian Yuen <brianyuen@outlook.com>
2021-01-17 13:04:19 -0800

    configure footer image

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,14 +3,14 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import footer from '../scss/footer.module.scss'
-
+import Image from './../components/image'
 
 
 const Footer = ( { siteTitle } ) => (
   <footer className={footer.footer}>
     <h3>Black Voices Matter.</h3>
     <Link to="/" className={footer.logo} title={siteTitle} >
-      {siteTitle}
+      <Image alt={siteTitle} filename="lgo-stand.png" />
     </Link>
     <h4>An initiative launched by</h4>
   </footer>
--- src/components/image.js
+++ src/components/image.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 /*
@@ -13,24 +13,37 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+const Image = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        images: allFile {
+          edges {
+            node {
+              relativePath
+              name
+              childImageSharp {
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
         }
       }
-    }
-  `)
+    `}
+    render={data => {
+      const image = data.images.edges.find(n => {
+        return n.node.relativePath.includes(props.filename);
+      });
+      if (!image) {
+        return null;
+      }
 
-  if (!data?.placeholderImage?.childImageSharp?.fluid) {
-    return <div>Picture not found</div>
-  }
-
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+      //const imageSizes = image.node.childImageSharp.sizes; sizes={imageSizes}
+      return <Img alt={props.alt} fluid={image.node.childImageSharp.fluid} />;
+    }}
+  />
+);
 
 export default Image
